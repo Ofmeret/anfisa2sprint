b288d1cc29fe40eb2a6d2dcb01d311ae7e019cf1
Aleksandr Makarov <Ofmeret@yandex.ru>
2025-04-20 18:58:08 +0400

    udpate 20.04

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,31 @@
+# from django.db.models import Q
 from django.shortcuts import render
+
+from ice_cream.models import IceCream
 
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    # Запрос:
+    # ice_cream_list = IceCream.objects.values(
+    #     'id', 'title', 'description'
+    #     ).filter(
+    #         # Делаем запрос, объединяя два условия
+    #         # через Q-объекты и оператор AND:
+    #         Q(is_published=True) &
+    #         (Q(is_on_main=True) | Q(title__contains='пломбир'))
+    #     )
+    # Полученный из БД QuerySet передаём в словарь контекста:
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'price', 'description'
+    ).filter(
+        # Проверяем, что
+        is_published=True,  # Сорт разрешён к публикации;
+        is_on_main=True,  # Сорт разрешён к публикации на главной странице;
+        category__is_published=True  # Категория разрешена к публикации.
+    )
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    # Словарь контекста передаём в шаблон, рендерим HTML-страницу:
+    return render(request, template, context)
--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+'''Представления для отображения деталей мороженого и списка мороженого.'''
+from django.shortcuts import get_object_or_404, render
+from django.db.models import Prefetch
+
+from ice_cream.models import IceCream, Topping
 
 
 def ice_cream_detail(request, pk):
+    '''Функция делает запрос информации к конкретному мороженному'''
     template = 'ice_cream/detail.html'
-    context = {}
-    return render(request, template, context)
+    ice_cream = get_object_or_404(
+        IceCream.objects.select_related('category', 'wrapper').only(
+            'title',
+            'price',
+            'description',
+            'category__title',
+            'wrapper__title'
+        ).prefetch_related(
+            Prefetch('toppings', queryset=Topping.objects.only('title'))
+        ).filter(is_published=True, category__is_published=True), pk=pk
+    )
+    return render(request, template, {'ice_cream': ice_cream})
 
 
 def ice_cream_list(request):
+    '''Функция  запроса для страницы Каталог мороженного'''
     template = 'ice_cream/list.html'
-    context = {}
+    ice_cream_list = IceCream.objects.select_related('category').filter(
+        is_published=True,
+        category__is_published=True
+    ).order_by('category')
+    context = {'ice_cream_list': ice_cream_list}
     return render(request, template, context)
